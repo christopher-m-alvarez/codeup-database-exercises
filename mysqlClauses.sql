7f3bce6ffddc6da113e354ba7cfa3ae30558181c
Christopher Alvarez <christopher.m.alvarez93@gmail.com>
2021-08-24 11:50:57 -0500

    where clause completed

---
+++ mysqlClauses.sql
@@ -0,0 +1,24 @@
+use employees;
+
+select * from employees.employees as e where e.last_name like '%tim%';
+
+select * from employees.employees as e where e.last_name like 'v%';
+
+select * from employees.employees as e where e.last_name like '%e';
+
+select * from employees.employees as e where e.last_name like '&gh&';
+
+
+
+
+select * from employees.employees as e
+where e.birth_date between '1964-06-02' and '1964-06-05';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name IN ('Herber', 'Dredge', 'Lipner', 'Baek');
+
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name = 'Herber' or last_name = 'Dredge';
---
+++ where_exercises.sql
@@ -0,0 +1,43 @@
+use employees;
+
+SELECT count(*)
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+
+# SELECT emp_no, first_name, last_name
+# FROM employees
+# WHERE first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya';
+
+
+SELECT count(*)
+FROM employees
+WHERE last_name like 'E%';
+
+
+SELECT count(*)
+FROM employees
+WHERE last_name like 'E%'
+   or last_name like '%e';
+
+SELECT count(*)
+FROM employees
+WHERE last_name like 'E%'
+  and last_name like '%e';
+
+
+
+select count(*)
+from employees e
+where e.gender = 'M'
+  and (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya');
+
+select count(*)
+from employees.employees as e
+where e.last_name like '%q%';
+
+select count(*)
+from employees.employees as e
+where e.last_name like '%q%'
+  and last_name not like '%qu%';
+
